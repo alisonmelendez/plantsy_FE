ec59ddae2aafc151a91796996ce2b082ae6ee5c5
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-03-08 15:10:43 -0500

    blank canvas

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Plantsy</h1>
+      </header>
+      <p>chicken little</p>
+    </div>
+  );
+}
+
+export default App;
