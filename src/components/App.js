4ef8aba3112dc5347d07e00a9d23fed6f821162a
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-03-10 13:10:02 -0500

    front end skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
 import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import PlantPage from "./PlantPage"
+import Plant from "./Plant"
 
 
 
 function App() {
+
+ const [plants, setPlants] = useState([])
+
+ const URL = "http://localhost:9292/plants/"
+
+ useEffect(() => {
+    fetch(URL)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPlants(data.results)
+      });
+}, []);
+
+const plantImg = plants.map((plant) => {
+  return <Plant key={plant.id} plant={plant}/>;
+})
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 style={{ textAlign : "center"}}>Plantsy</h1>
       </header>
-      <p>chicken little</p>
+      <div>
+      {plants.length === 0 ? <p>no results</p> :plantImg}
+
+        <Route path="/Plants/:name">
+              <PlantPage/>
+        </Route>
+
+      </div>
+
     </div>
   );
 }
